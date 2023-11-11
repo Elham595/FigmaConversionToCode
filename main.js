b3afaecb05c0979a388129f509ba0b555359c18b
Elham <elhambalbaa99@gmail.com>
2023-11-11 16:32:32 +0200

    Convert a Figma File into Code HTML&CSS

---
+++ main.js
@@ -0,0 +1,9 @@
+let nav = document.querySelector("nav");
+
+window.addEventListener("scroll", ()=>{
+    if(window.pageYOffset > 100){
+        nav.classList.add("scrolled")
+    }else{
+        nav.classList.remove("scrolled")
+    }
+})
